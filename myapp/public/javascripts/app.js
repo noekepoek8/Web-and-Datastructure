ab5ce99f0adb360ee9c54ee9f89b532b6fa4ff7d
noekepoek8 <anoek.winkelman@live.nl>
2018-12-06 16:55:42 +0100

    ready for pins to put in

--- myapp/public/javascripts/app.js
+++ myapp/public/javascripts/app.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-    $('.selector-inner').click(function () {
+    $('.selectionBtn').click(function () {
         console.log($(this));
+        let color = $(this).css('background-color');
+        console.log(color);
         $('.inputPeg').css('background-color', 'pink');
     });
+
+    // var express = require("express");
+    // var http = require("http");
     
-    var express = require("express");
-    var http = require("http");
+    // var port = process.argv[2];
+    // var app = express();
     
-    var port = process.argv[2];
-    var app = express();
-    
-    app.use(express.static(__dirname + "/public"));
-    http.createServer(app).listen(port);
+    // app.use(express.static(__dirname + "/public"));
+    // http.createServer(app).listen(port);
 });
