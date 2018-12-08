e3d0d8c097fa46c5a5b0a8b7af21d4cb24226050
noekepoek8 <anoek.winkelman@live.nl>
2018-12-08 23:48:33 +0100

    tweaks

--- myapp/test.js
+++ myapp/test.js
@@ -4,6 +4,15 @@ var http = require("http");
 var port = process.argv[2];
 var app = express();
 
+app.get('/', function(req, res) {
+    res.sendFile('splash.html', {root: "./public"});
+});
+
+app.get('/game/:id', function(req, res) {
+    res.send('you requested to see a profile with the id of ' + req.params.id);
+});
+
 app.use(express.static(__dirname + "/public"));
 http.createServer(app).listen(port);
+
 
